72ce18f36b0a8288add80f78c7b81d22ccc75425
JoakimIEngstrom <joakim.engstrom@utb.ecutbildning.se>
2022-01-26 21:07:06 +0100

    Push för att prova göra det mesta i JS istället.

--- scripts.js
+++ scripts.js
@@ -5,14 +5,26 @@ let firstCard, secondCard;
 
 function flipCard() {
   this.classList.toggle(`flip`);
-}
 
-if (!hasFlippedCard) {
-  // first click
-  hasFlippedCard = true;
-  firstCard = this;
+  if (!hasFlippedCard) {
+    // first click
+    hasFlippedCard = true;
+    firstCard = this;
+  } else {
+    // second click
+    hasFlippedCard = false;
+    secondCard = this;
 
-  console.log({ hasFlippedCard, flipCard });
+    //check if they match
+    if (firstCard.dataset.framework === secondCard.dataset.framework) {
+      // it´s a match
+      firstCard.removeEventListener(`click`, flipCard);
+      secondCard.removeEventListener(`click`, flipCard);
+    } else {
+      firstCard.classList.remove(`flip`);
+      secondCard.classList.remove(`flip`);
+    }
+  }
 }
 
 cards.forEach((card) => card.addEventListener(`click`, flipCard));
